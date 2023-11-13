b4d947f5f3e6f085eeae63bfc7055687d925c43b
Mohammad Al-Azawi <azawi.gt@gmail.com>
2023-11-13 14:04:31 +0000

    7

---
+++ comp377.py
@@ -0,0 +1,16 @@
+def ReadInt():
+    x = input("Enter an Integer: ")
+    x = int (x)
+    return x
+
+def Max2(x, y):
+    if x>y: 
+        return x
+    else:
+        return y
+    
+def add(x, y):
+    return (x+y)
+
+def mul(x, y):
+    return (x*y)
